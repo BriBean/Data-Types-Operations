75924ebd25705ddcefc1e61cb8a6d4662d2cb966
Briannaaa Bean <86678213+BriBean@users.noreply.github.com>
2021-07-01 17:35:15 +0000

    git push

--- primary.py
+++ primary.py
@@ -1,5 +1,5 @@
-#author: <author here>
-# date: <date here>
+#author: <Brianna Blue>
+# date: <7/1/21>
 
 # --------------- Section 1 --------------- #
 # ---------- Integers and Floats ---------- #
@@ -11,42 +11,44 @@
 #       1 - create a print statement that prints the sum of two numbers
 #       2 - create a print statement that prints the sum of three numbers
 #       3 - create a print statement the prints the sum of two negative numbers
-
-
-
+print(22 + 14)
+print(30 + 10 + 2.5)
+print(-4.0 + -2.0)
 # subtraction
 #   instructions
 #       1 - create a print statement that prints the difference of two numbers
 #       2 - create a print statement that prints the difference of three numbers
 #       3 - create a print statement the prints the difference of two negative numbers
-
-
-
+print()
+print(100 - 60)
+print(50 - 26 - 1.5)
+print(-35 - -17)
 # multiplication and true division
 #   instructions
 #       1 - create a print statement the prints the product of two numbers
 #       2 - create a print statement that prints the dividend of two numbers
 #       3 - create a print statement that evaluates an operation using both multiplication and division
-
-
-
+print()
+print(9 * 6)
+print(81 / 9)
+print(5 * 7 / 2)
 # floor division
 #   instructions
 #       1 - using floor division, print the dividend of two numbers.
-
-
+print()
+print(72 // -8)
 
 # exponentiation
 #   instructions
 #       1 - using exponentiation, print the power of two numbers
-
-
+print()
+print(11 ** 6)
 
 # modulus
 #   instructions
 #       1 - using modulus, print the remainder of two numbers
-
-
+print()
+print(45 % 7)
 # --------------- Section 2 --------------- #
 # ---------- String Concatenation --------- #
 
@@ -55,15 +57,19 @@
 #       1 - print the concatenation of your first and last name
 #       2 - print the concatenation of five animals you like
 #       3 - print the concatenation of each word in a phrase
-
-
-
+print()
+print('Brianna' + 'Blue')
+print('otters'+'cats'+'dogs'+'lions'+'elephants')
+print('I'+'love'+'buttery'+'mushrooms')
 # duplication
 #   instructions
-#       1 - print the duplpication of your first 5 times
+#       1 - print the duplpication of your first name 5 times
 #       2 - print the duplication of a song you like 10 times
-
-
+print()
+print('Brianna'* 5)
+print('Time Machine'* 10)
 # concatenation and duplpication
 #   instructions
 #       1 - print the concatenation of two strings duplicated 3 times each
+print()
+print('french'*3 + 'fries'*3 )
